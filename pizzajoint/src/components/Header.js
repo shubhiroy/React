0ac4b0d373c9b9c2255dee58643ab1e150b1b298
shubhiroy <shubham09roy@gmail.com>
2020-09-18 23:53:50 +0530

    Added svg animation

--- pizzajoint/src/components/Header.js
+++ pizzajoint/src/components/Header.js
@@ -1,32 +1,65 @@
-import React from 'react';
-import { motion } from 'framer-motion';
+import React from "react";
+import { motion } from "framer-motion";
+
+const svgVariant = {
+  hidden: { rotate: -180 },
+  visible: {
+    rotate: 0,
+    transition: { duration: 1 },
+  },
+};
+
+const pathVariant = {
+  hidden: {
+    opacity: 0,
+    pathLength: 0,
+  },
+  visible: {
+    opacity: 1,
+    pathLength: 1,
+    transition: {
+      duration: 2,
+      ease: "easeInOut",
+    },
+  },
+};
 
 const Header = () => {
   return (
     <header>
       <div className="logo">
-        <svg className="pizza-svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
-          <path
+        <motion.svg
+          className="pizza-svg"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 100 100"
+          variants={svgVariant}
+          initial="hidden"
+          animate="visible"
+        >
+          <motion.path
             fill="none"
             d="M40 40 L80 40 C80 40 80 80 40 80 C40 80 0 80 0 40 C0 40 0 0 40 0Z"
+            variants={pathVariant}
           />
-          <path
+          <motion.path
             fill="none"
             d="M50 30 L50 -10 C50 -10 90 -10 90 30 Z"
+            variants={pathVariant}
           />
-        </svg>
+        </motion.svg>
       </div>
-        <motion.div className="title"
+      <motion.div
+        className="title"
         initial={{ y: -250 }}
-        animate={{y: -10 }}
-        // Default type is spring but if anytihing else like stiffness is mentioned then we have to declare the type. 
+        animate={{ y: -10 }}
+        // Default type is spring but if anytihing else like stiffness is mentioned then we have to declare the type.
         // stiffness property can only be used for spring type
-        transition={{ delay: 0.2, type: 'spring', stiffness: 120}} 
-        >
-          <h1>Pizza Joint</h1>
-        </motion.div>
+        transition={{ delay: 0.2, type: "spring", stiffness: 120 }}
+      >
+        <h1>Pizza Joint</h1>
+      </motion.div>
     </header>
-  )
-}
+  );
+};
 
 export default Header;
