aa714e0f4057df40f2b06e7b51f049f4a607bda4
shubhiroy <shubham09roy@gmail.com>
2020-09-18 12:30:57 +0530

    Add hover animation to list items

--- pizzajoint/src/components/Home.js
+++ pizzajoint/src/components/Home.js
@@ -15,9 +15,15 @@ const Home = () => {
         Welcome to Pizza Joint
       </h2>
       <Link to="/base">
-        <button>
+        <motion.button
+        whileHover={{ 
+          scale: 1.1,
+          textShadow: '0px 0px 8px rgb(255, 255, 255)',
+          boxShadow: '0px 0px 8px rgb(255, 255, 255)',
+           }}
+        >
           Create Your Pizza
-        </button>
+        </motion.button>
       </Link>
     </motion.div>
   )
