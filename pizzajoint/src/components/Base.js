78e4f1ca8ab098a5a005b1da6241c4d42171df6c
shubhiroy <shubham09roy@gmail.com>
2020-09-18 19:47:14 +0530

    Add variants

--- pizzajoint/src/components/Base.js
+++ pizzajoint/src/components/Base.js
@@ -2,15 +2,44 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { motion } from "framer-motion";
 
+const containerVariants = {
+  hidden: {
+    opacity: 0,
+    x: "100vw",
+  },
+
+  visible: {
+    opacity: 1,
+    x: 0,
+    transition: { 
+      type: "spring", 
+      dealay: 0.5 
+    },
+  },
+};
+
+const nextVariants = {
+  hidden: {
+    x: "-100vw"
+  },
+  visible: {
+    x: 0,
+    transition: { 
+      type: "spring", 
+      stiffness: 120 
+    }
+  }
+}
+
 const Base = ({ addBase, pizza }) => {
   const bases = ["Classic", "Thin & Crispy", "Thick Crust"];
 
   return (
     <motion.div
       className="base container"
-      initial={{ x: "100vw" }}
-      animate={{ x: 0 }}
-      transition={{ type: "spring", delay: 0.5 }}
+      variants={containerVariants}
+      initial="hidden"
+      animate="visible"
     >
       <h3>Step 1: Choose Your Base</h3>
       <ul>
@@ -32,9 +61,13 @@ const Base = ({ addBase, pizza }) => {
       {pizza.base && (
         <motion.div
           className="next"
-          initial={{ x: "-100vw" }}
-          animate={{ x: 0 }}
-          transition={{ type: "spring", stiffness: 120 }}
+          variants={nextVariants}
+          // Animaiton attribute propogated down through the DOM 
+          // i.e. from parent (containerVariants) to child (nextVariants)
+          // It only works if the properties of both 
+          // parent and child have same name like hidden and visible in this scenario  
+          // initial="hidden"
+          // animate="visible"
         >
           <Link to="/toppings">
             <motion.button
